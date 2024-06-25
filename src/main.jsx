ab636b9843ec014501b4eff8e3cf35b9aa862a9e
Sachintambeshwar <sachintambeshwar496@gmail.com>
2024-06-25 12:58:22 +0530

    package chnages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import App from "./App.jsx";
 import "./index.css";
 import Layout from "./components/Layout.jsx";
 import Contact from "./components/Contact.jsx";
